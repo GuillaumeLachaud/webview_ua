4ab51274ff2bc3f61d8ed73e2bf30e7682dfb975
Guillaume LACHAUD <guillaume.lachaud@gmail.com>
2014-03-04 09:51:02 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+WebView UA
+==========
+
+A very simple app that just displays the User-Agent of the WebView of the device it runs on.
---
+++ app/src/main/java/com/example/webviewua/app/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.webviewua.app;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView view = (WebView) findViewById(R.id.webView);
+        String ua = view.getSettings().getUserAgentString() ;
+        ((TextView) findViewById(R.id.userAgentTextView)).setText(ua);
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            android.text.ClipboardManager clipboard = (android.text.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            clipboard.setText(ua);
+        } else {
+            android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            android.content.ClipData clip = android.content.ClipData.newPlainText(getString(R.string.app_name), ua);
+            clipboard.setPrimaryClip(clip);
+        }
+
+        Toast.makeText(this, getString(R.string.copiedToClipboard), Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
